5b15b1179613299eb97f6fce6d97e11fcbe9dd81
Anton Potapov <anton.potapoff92@gmail.com>
2022-11-18 02:33:33 +0700

    error correction

--- main.py
+++ main.py
@@ -1,71 +1,74 @@
-import time
 import json
 import requests
-from pprint import pprint
-import time
 from tqdm import tqdm
-from itertools import chain
+
+with open('token.txt', 'r') as file_object:
+    vk_token = file_object.read().strip()
 
 
-def main(user_ids, ya_token):
-    with open('token.txt', 'r') as file_object:
-        vk_token = file_object.read().strip()
+class VkUser:
+    url = 'https://api.vk.com/method/photos.get'
 
-    def photos_get():
-        url = 'https://api.vk.com/method/photos.getProfile'
-        params = {'extended': '1',
-                  'count': '3',
-                  'rev': '1',
-                  'photo_sizes': '1',
-                  'user_ids': user_ids,
-                  'access_token': vk_token,
-                  'v': '5.131'
-                  }
-        res = requests.post(url, params=params)
-        photos = []
+    def __init__(self, user_id, vk_token):
+        self.params = {'owner_id': user_id,
+                       'album_id': 'profile',
+                       'access_token': vk_token,
+                       'v': '5.131',
+                       'extended': '1',
+                       'photo_sizes': '1',
+                       'count': 3,
+                       }
+
+    def photos_get(self):
         name_photos = []
-        name_files = []
-        a = res.json()
-        for i in tqdm(a['response']['items']):
-            for p in i['sizes']:
-                if p['type'] != 'z':
+        urls = []
+        res = requests.post(self.url, params=self.params)
+        reply = res.json()
+        for info in tqdm(reply['response']['items']):
+            for form in info['sizes']:
+                if form['type'] != 'z':
                     continue
                 else:
-                    url_photo = p['url']
-                    size = p['type']
-                    file_name = i['likes']['count']
-                    photos.append(url_photo)
+                    url_photo = form['url']
+                    size = form['type']
+                    file_name = info['likes']['count']
                     name_dict = {'file_name': file_name, 'size': size}
                     name_photos.append(name_dict)
                     url_dict = {'url': url_photo, 'name': file_name}
-                name_files.append(url_dict)
+                urls.append(url_dict)
 
-        with open('photos.json', 'w') as f:
-            json.dump(name_photos, f)
+        with open('photos.json', 'w') as file:
+            json.dump(name_photos, file)
+        return urls
 
-        def create_folder():
-            headers = {'Content-Type': 'application/json',
-                       'Accept': 'application/json',
-                       'Authorization': f'OAuth {ya_token}'}
-            link_folder = 'https://cloud-api.yandex.net/v1/disk/resources'
-            link_upload = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-            name_folder = "vk_photo"
-            file_path = 'vk_photo/'
-            requests.put(f'{link_folder}?path={name_folder}', headers=headers)
 
-            def upload_photo():
-                for h in name_files:
-                    file_path = (f'vk_photo/{h["name"]}')
-                    params = {'path': (file_path), 'url': h['url']}
-                    response = requests.post(link_upload, params=params, headers=headers)
-                pprint(response.status_code)
+class YaDiskUser:
 
-            return upload_photo()
+    def __init__(self, ya_token: str, urls):
+        self.ya_token = ya_token
+        self.headers = {'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': f'OAuth {self.ya_token}'}
+        self.urls = urls
 
-        return create_folder()
+    def create_folder(self):
+        link_folder = 'https://cloud-api.yandex.net/v1/disk/resources'
+        name_folder = "vk_photo"
+        requests.put(f'{link_folder}?path={name_folder}', headers=self.headers)
 
-    return photos_get()
+    def upload_photo(self):
+        link_upload = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        for url in self.urls:
+            file_path = (f'vk_photo/{url["name"]}')
+            params = {'path': (file_path), 'url': url['url']}
+            res = requests.post(link_upload, params=params, headers=self.headers)
+        print(res.status_code)
 
 
 if __name__ == '__main__':
-    main('95301904', 'ya_token')
+    user_id = 1
+    vk_client = VkUser(user_id, vk_token)
+    ya_token = ""
+    uploader = YaDiskUser(ya_token, vk_client.photos_get())
+    uploader.create_folder()
+    uploader.upload_photo()
